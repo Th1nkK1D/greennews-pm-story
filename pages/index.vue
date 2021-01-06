8757c46bd86cdc893eca3ef390ce1c5e1c117aa9
Th1nkK1D <witheep@gmail.com>
2021-01-06 18:33:03 +0700

    add basic language switch

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,26 @@
 <template>
-  <div>
+  <div class="p-4">
+    <p class="text-right">
+      switch to
+      <nuxt-link
+        class="text-blue-600 hover:underline"
+        :to="switchLocalePath(inactiveLanguage)"
+      >
+        {{ inactiveLanguage.toUpperCase() }}
+      </nuxt-link>
+    </p>
     <div class="text-3xl font-bold">{{ $t('intro.title') }}</div>
     <pre>{{ $t('calltoaction') }}</pre>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  computed: {
+    inactiveLanguage() {
+      return this.$i18n.locales.find((locale) => locale !== this.$i18n.locale)
+    },
+  },
+})
+</script>
