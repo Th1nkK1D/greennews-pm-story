0a7726bb50f66fb70b21756627b5a29d4d474727
Th1nkK1D <witheep@gmail.com>
2021-01-13 22:52:46 +0700

    Add random particle animation with animejs

---
+++ components/pollution-background.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="relative overflow-hidden"><slot /></div>
+</template>
+
+<script>
+import anime from 'animejs'
+
+const PARTICLE_AMOUNT = 15
+
+export default {
+  mounted() {
+    for (let i = 0; i < PARTICLE_AMOUNT; i++) {
+      const particle = document.createElement('div')
+      particle.classList.add('particle')
+      particle.style.top = `${Math.random() * 100}%`
+      particle.style.left = `${Math.random() * 100}%`
+      particle.style.scale = Math.random() + 0.5
+
+      this.$el.appendChild(particle)
+
+      anime({
+        targets: particle,
+        translateY: 10 + Math.random() * 10,
+        duration: 1000 + Math.random() * 1000,
+        loop: true,
+        direction: 'alternate',
+        easing: 'easeInOutSine',
+        delay: Math.random() * 1000,
+      })
+    }
+  },
+}
+</script>
+
+<style>
+.particle {
+  position: absolute;
+  width: 23px;
+  height: 23px;
+  background: #c4c4c4;
+  filter: blur(10px);
+  z-index: -1;
+}
+</style>
--- components/sections/intro.vue
+++ components/sections/intro.vue
@@ -1,14 +1,16 @@
 <template>
   <section class="flex flex-col">
-    <!-- Landing -->
-    <div class="h-screen flex">
-      <div class="max-w-lg m-auto text-center">
-        <h2 class="text-36">{{ $t('intro.subtitle') }}</h2>
-        <h1 class="text-72 font-bold">{{ $t('intro.title') }}</h1>
+    <PollutionBackground>
+      <!-- Landing -->
+      <div class="h-screen flex">
+        <div class="max-w-lg m-auto text-center">
+          <h2 class="text-36">{{ $t('intro.subtitle') }}</h2>
+          <h1 class="text-72 font-bold">{{ $t('intro.title') }}</h1>
+        </div>
       </div>
-    </div>
-    <!-- End of Landing -->
-    <div ref="enterView">test enter view</div>
+      <!-- End of Landing -->
+      <div ref="enterView">test enter view</div>
+    </PollutionBackground>
   </section>
 </template>
 
