ed93fcb5c067e19fc0394f1d35ea4d8fa99684eb
Th1nkK1D <witheep@gmail.com>
2021-01-13 21:24:45 +0700

    Add styleguide to tailwind config

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="p-4">
     <LanguageSwitch class="text-right" />
-    <div class="text-3xl font-bold">{{ $t('intro.title') }}</div>
+    <div class="text-72 font-bold">{{ $t('intro.title') }}</div>
     <pre>{{ $t('calltoaction') }}</pre>
   </div>
 </template>
+
+<style>
+html {
+  @apply font-sans;
+  @apply text-18;
+}
+</style>
