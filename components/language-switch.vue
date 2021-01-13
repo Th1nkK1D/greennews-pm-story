f508cab219c450509ba45682befe7b0843672aa5
Th1nkK1D <witheep@gmail.com>
2021-01-13 21:00:09 +0700

    Split language switch to seperated component

---
+++ components/language-switch.vue
@@ -0,0 +1,21 @@
+<template>
+  <p class="text-right">
+    switch to
+    <nuxt-link
+      class="text-blue-600 hover:underline"
+      :to="switchLocalePath(inactiveLanguage)"
+    >
+      {{ inactiveLanguage.toUpperCase() }}
+    </nuxt-link>
+  </p>
+</template>
+
+<script>
+export default {
+  computed: {
+    inactiveLanguage() {
+      return this.$i18n.locales.find((locale) => locale !== this.$i18n.locale)
+    },
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,7 @@
 <template>
   <div class="p-4">
-    <p class="text-right">
-      switch to
-      <nuxt-link
-        class="text-blue-600 hover:underline"
-        :to="switchLocalePath(inactiveLanguage)"
-      >
-        {{ inactiveLanguage.toUpperCase() }}
-      </nuxt-link>
-    </p>
+    <LanguageSwitch class="text-right" />
     <div class="text-3xl font-bold">{{ $t('intro.title') }}</div>
     <pre>{{ $t('calltoaction') }}</pre>
   </div>
 </template>
-
-<script lang="ts">
-import Vue from 'vue'
-export default Vue.extend({
-  computed: {
-    inactiveLanguage() {
-      return this.$i18n.locales.find((locale) => locale !== this.$i18n.locale)
-    },
-  },
-})
-</script>
