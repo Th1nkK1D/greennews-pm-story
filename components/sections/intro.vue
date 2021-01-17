89b7351683b8cd2bb474c7bc2d933f4b4a7f14ce
Th1nkK1D <witheep@gmail.com>
2021-01-17 14:42:48 +0700

    Update intro and part 1 content

--- components/sections/intro.vue
+++ components/sections/intro.vue
@@ -2,27 +2,31 @@
   <section class="flex flex-col text-center">
     <PollutionBackground>
       <div class="h-screen flex">
+        <div class="max-w-2xl m-auto flex flex-col space-y-8">
+          <p class="text-h5">
+            {{ $t('intro.prologue1') }}
+            <br />
+            {{ $t('intro.prologue2') }}
+          </p>
+          <p class="text-h5">{{ $t('intro.prologue3') }}</p>
+          <p class="text-h5">{{ $t('intro.prologue4') }}</p>
+        </div>
+      </div>
+      <div class="h-screen flex">
         <div class="max-w-lg m-auto flex flex-col space-y-24">
-          <h1 class="text-72 font-bold">{{ $t('intro.title') }}</h1>
-          <div class="flex flex-row space-x-12 text-white">
-            <button class="p-2 bg-green-1 rounded w-full">
+          <h1 class="text-h1 font-bold">{{ $t('intro.title') }}</h1>
+          <div class="flex flex-row space-x-12">
+            <button
+              class="p-1 bg-green-1 rounded-xl w-full text-white font-bold"
+            >
               {{ $t('intro.button1') }}
             </button>
-            <button class="p-2 bg-green-1 rounded w-full">
+            <button
+              class="p-1 text-green-1 bg-green-3 rounded-xl w-full font-bold"
+            >
               {{ $t('intro.button2') }}
             </button>
           </div>
-        </div>
-      </div>
-      <div class="max-w-xl mx-auto flex flex-col space-y-48 pb-48">
-        <p class="text-64 font-bold">
-          {{ $t('intro.prologue1') }}
-        </p>
-        <div>
-          <p class="text-24">{{ $t('intro.prologue2') }}</p>
-          <p class="text-48">
-            {{ $t('intro.prologue3') }}
-          </p>
         </div>
       </div>
     </PollutionBackground>
--- components/sections/part1.vue
+++ components/sections/part1.vue
@@ -1,5 +1,8 @@
 <template>
-  <section class="flex flex-col bg-gray-2 py-48 space-y-48">
+  <section class="flex flex-col bg-black-4 py-48 space-y-48">
+    <div class="p-2 bg-green-3 text-green-1 text-h6 font-bold mx-auto -my-24">
+      รู้จักปัญหา: หน้าตา
+    </div>
     <div class="space-y-24">
       <img
         src="https://via.placeholder.com/800x400?text=motocycler_illustration"
@@ -7,9 +10,9 @@
         class="w-full max-w-2xl m-auto"
       />
       <div class="max-w-lg m-auto space-y-8">
-        <p>{{ $t('chapter1.story1') }}</p>
-        <p>{{ $t('chapter1.story2') }}</p>
-        <p>{{ $t('chapter1.story3') }}</p>
+        <p class="text-indent">{{ $t('chapter1.story1') }}</p>
+        <p class="text-indent">{{ $t('chapter1.story2') }}</p>
+        <p class="text-indent">{{ $t('chapter1.story3') }}</p>
       </div>
     </div>
     <div class="space-y-24">
@@ -18,7 +21,20 @@
         alt=""
         class="w-full max-w-2xl m-auto"
       />
-      <p class="max-w-lg m-auto">{{ $t('chapter1.factbox') }}</p>
+      <div class="max-w-lg m-auto space-y-8">
+        <p class="text-indent">{{ $t('chapter1.factbox') }}</p>
+        <p class="text-indent">{{ $t('chapter1.story4') }}</p>
+      </div>
+    </div>
+    <div class="space-y-4 max-w-2xl m-auto">
+      <img
+        src="https://via.placeholder.com/800x400?text=pm2.5_heatmap"
+        alt=""
+        class="w-full"
+      />
+      <p class="text-right text-body2">
+        {{ $t('chapter1.datavisual.source') }}
+      </p>
     </div>
   </section>
 </template>
