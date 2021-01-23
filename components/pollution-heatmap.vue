85f9dc359ebaf27e6cd83b380a0ecd23452e0a63
Th1nkK1D <witheep@gmail.com>
2021-01-23 15:35:08 +0700

    Add heatmap tooltip and animation

--- components/pollution-heatmap.vue
+++ components/pollution-heatmap.vue
@@ -12,22 +12,50 @@
         class="flex-1 flex flex-row space-x-1 md:space-x-2"
       >
         <div
-          v-for="(bgColor, dayIndex) in month"
+          v-for="({ value, bgColor }, dayIndex) in month"
           :key="dayIndex"
-          class="flex-1 rounded"
-          :class="bgColor"
-        />
+          class="flex-1"
+        >
+          <div class="relative">
+            <div
+              v-if="
+                hovered.dayIndex === dayIndex &&
+                hovered.monthIndex === monthIndex
+              "
+              class="absolute bottom-1 bg-black text-white rounded rounded-bl-none px-2 py-1 text-body2"
+            >
+              {{ value }}
+            </div>
+          </div>
+          <div
+            v-if="value"
+            class="dayfill w-full h-full rounded transform transition-transform hover:scale-110 duration-100 ease-in-out"
+            :class="bgColor"
+            @mouseenter="hovered = { dayIndex, monthIndex }"
+            @mouseleave="hovered = { dayIndex: null, monthIndex: null }"
+          />
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import anime from 'animejs'
+import enterView from 'enter-view'
 import log from '../assets/data/bkk-pm-log.json'
 
 const MAX_DAY_IN_MONTH = 31
 
 export default {
+  data() {
+    return {
+      hovered: {
+        dayIndex: null,
+        monthIndex: null,
+      },
+    }
+  },
   computed: {
     months() {
       return this.$i18n.locale === 'th'
@@ -37,12 +65,33 @@ export default {
     data() {
       return log.map((month) =>
         month
-          .map((value) =>
-            value >= 90 ? 'bg-red' : value >= 50 ? 'bg-orange' : 'bg-green-3'
+          .map((value) => ({
+            value,
+            bgColor:
+              value >= 90 ? 'bg-red' : value >= 50 ? 'bg-orange' : 'bg-green-3',
+          }))
+          .concat(
+            new Array(MAX_DAY_IN_MONTH - month.length).fill({
+              bgColor: 'bg-none',
+            })
           )
-          .concat(new Array(MAX_DAY_IN_MONTH - month.length).fill('bg-none'))
       )
     },
+  },
+  mounted() {
+    const animation = anime({
+      targets: this.$el.querySelectorAll('.dayfill'),
+      opacity: [0, 1],
+      delay: (_, i) => i * 10,
+      autoplay: false,
+    })
+
+    enterView({
+      selector: [this.$el],
+      enter: () => animation.play(),
+      once: true,
+      offset: 0.2,
+    })
   },
 }
 </script>
