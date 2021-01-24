e65c52f2b95a573dfd13ed9b7200eca3eeb28846
Th1nkK1D <witheep@gmail.com>
2021-01-24 18:29:45 +0700

    Link greennews logo to homepage

--- pages/index.vue
+++ pages/index.vue
@@ -4,11 +4,15 @@
     :class="bgColor"
   >
     <ProgressBar class="fixed left-0 right-0 z-10" :progress="readProgress" />
-    <img
-      src="~/assets/images/logo-greennews.png"
-      alt="Greennews"
-      class="absolute top-4 left-4 md:top-8 md:left-8 z-10"
-    />
+    <a
+      href="https://greennews.agency/"
+      target="_blank"
+      rel="noopener noreferrer"
+      ><img
+        src="~/assets/images/logo-greennews.png"
+        alt="Greennews"
+        class="absolute top-4 left-4 md:top-8 md:left-8 z-10"
+    /></a>
     <LanguageSwitch class="absolute top-4 right-4 md:top-8 md:right-8 z-10" />
     <Intro />
     <ViewWatch @enter="bgColor = 'bg-white'" @exit="bgColor = ''">
